dc456427bc5c748c74a8155d89ef92ef5d6b5ee8
hampus-code <hampusss96@gmail.com>
2024-11-27 14:11:31 +0100

    Add Repository annotation on CustomMovieRepository interface

--- src/main/kotlin/com/hampus_code/labb_1/repository/CustomMovieRepository.kt
+++ src/main/kotlin/com/hampus_code/labb_1/repository/CustomMovieRepository.kt
@@ -1,4 +1,9 @@
 package com.hampus_code.labb_1.repository
 
-interface CustomMovieRepository {
+import com.hampus_code.labb_1.model.CustomMovie
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CustomMovieRepository: JpaRepository<CustomMovie, Long> {
 }
