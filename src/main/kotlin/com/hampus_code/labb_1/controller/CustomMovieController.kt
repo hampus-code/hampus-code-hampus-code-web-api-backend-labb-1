8d3287f41b274e383024c26888522741c2025794
hampus-code <hampusss96@gmail.com>
2024-11-27 14:19:04 +0100

    Add a GET in CustomMovieController

--- src/main/kotlin/com/hampus_code/labb_1/controller/CustomMovieController.kt
+++ src/main/kotlin/com/hampus_code/labb_1/controller/CustomMovieController.kt
@@ -1,4 +1,29 @@
 package com.hampus_code.labb_1.controller
 
-class CustomMovieController {
+import com.hampus_code.labb_1.model.CustomMovie
+import com.hampus_code.labb_1.repository.CustomMovieRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/movie")
+class CustomMovieController(
+    private val customMovieRepository: CustomMovieRepository
+) {
+    
+    @GetMapping
+    fun getMovieById(@RequestParam("id") id: Long): ResponseEntity<CustomMovie> {
+
+        val foundOptionalMovie = customMovieRepository.findById(id)
+
+        if (foundOptionalMovie.isPresent) {
+            val notOptionalMovie = foundOptionalMovie.get()
+            return ResponseEntity.status(200).body(notOptionalMovie)
+        }
+
+        return ResponseEntity.notFound().build()
+    }
 }
