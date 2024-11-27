f627f0241f099ff3e31edc3b634acb418fbc0226
hampus-code <hampusss96@gmail.com>
2024-11-27 14:08:59 +0100

    Add properties in the CustomMovie class, also add Entity and Id annotations

--- src/main/kotlin/com/hampus_code/labb_1/model/CustomMovie.kt
+++ src/main/kotlin/com/hampus_code/labb_1/model/CustomMovie.kt
@@ -1,4 +1,20 @@
 package com.hampus_code.labb_1.model
 
-class CustomMovie {
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class CustomMovie(
+    val title: String = "",
+    val director: String = "",
+    val actor: String = "",
+    val isEnabled: Boolean = true
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null
+
 }
